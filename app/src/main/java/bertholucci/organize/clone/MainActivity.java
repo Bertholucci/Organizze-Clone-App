f565abb3fdcf88f7d85cafe247a6696fcb898e29
Bertholucci <38389969+Matheus-Bertholucci@users.noreply.github.com>
2020-07-24 14:50:25 -0300

    Fixed the image looking. Fixed the size of TextViews and ImageViews.
    :construction: Added the CadastroActivity
    :construction: Added the LoginActivity
    
    :package: Finished the configuration of Slides using Material-Intro

--- app/src/main/java/bertholucci/organize/clone/MainActivity.java
+++ app/src/main/java/bertholucci/organize/clone/MainActivity.java
@@ -2,17 +2,24 @@ package bertholucci.organize.clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+import bertholucci.organize.clone.activity.CadastroActivity;
+import bertholucci.organize.clone.activity.LoginActivity;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         /* Not necessary because the IntroActivity statement
             setContentView(R.layout.activity_main);
          */
@@ -23,6 +30,7 @@ public class MainActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
             .background(android.R.color.white)
                 .fragment(R.layout.intro_1)
+                .canGoBackward(false)
                 .build()
         );
 
@@ -44,11 +52,19 @@ public class MainActivity extends IntroActivity {
                 .build()
         );
 
-        /* Erase the back and forward button
-            It was just a test :(
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_5_cad)
+                .canGoForward(false)
+                .build()
+        );
+    }
 
-            setButtonBackVisible(false);
-            setButtonNextVisible(false);
-        */
+    public void botaoEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void botaoCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
---
+++ app/src/main/java/bertholucci/organize/clone/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package bertholucci.organize.clone.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bertholucci.organize.clone.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
